23909fc11b8d878223625b6483f18b886139fc34
akasheyy <vadakkangaraakash@gmail.com>
2026-01-12 00:53:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://connecto-frontend.vercel.app/"
+      "https://connecto-frontend.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"]
